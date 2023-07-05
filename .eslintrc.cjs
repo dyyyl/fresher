28d052dd391844ce170e50caf8b24abd8d98b199
dyyyl <hey@dyyyl.rocks>
2023-07-05 16:34:04 -0400

    update prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,26 +3,25 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'prettier'],
+  extends: ["eslint:recommended", "prettier"],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    semi: ["error", "always"],
   },
 };
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import playwright from 'playwright';
+import { program } from "commander";
+import playwright from "playwright";
 
 program
-  .name('fresher')
-  .description('A CLI utility to launch a fresh browser instance.')
-  .version('0.1.0')
+  .name("fresher")
+  .description("A CLI utility to launch a fresh browser instance.")
+  .version("0.1.0")
   .requiredOption('-b, --browser <"chromium" | "firefox" | "webkit">')
-  .option('-u, --url <string>');
+  .option("-u, --url <string>");
 
 program.parse();
 
@@ -19,7 +19,7 @@ async function main() {
   });
 
   const page = await browser.newPage();
-  await page.goto(options.url || 'https://www.duckduckgo.com');
+  await page.goto(options.url || "https://www.duckduckgo.com");
 }
 
 main();
