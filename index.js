71148b4c73c3f5c22a1f9b6b5ae03429097bfcb2
dyyyl <hey@dyyyl.rocks>
2023-07-06 09:00:12 -0400

    more meta ft: dj start script

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .name("fresher")
   .description("A CLI utility to launch a fresh browser instance.")
-  .version("0.1.2")
+  .version("0.1.3")
   .requiredOption(
     '-b, --browser <"chromium" | "firefox" | "webkit">',
     "browser to launch"
