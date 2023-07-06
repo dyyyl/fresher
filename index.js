eddefe4460207bba44da655a86f8ff2b37043f60
dyyyl <hey@dyyyl.rocks>
2023-07-06 08:49:48 -0400

    remove module

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .name("fresher")
   .description("A CLI utility to launch a fresh browser instance.")
-  .version("0.1.1")
+  .version("0.1.2")
   .requiredOption(
     '-b, --browser <"chromium" | "firefox" | "webkit">',
     "browser to launch"
