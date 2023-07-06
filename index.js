d5b6f40e823a342a2cb703c4c4bb25f6cfd3cb0c
dyyyl <hey@dyyyl.rocks>
2023-07-06 09:03:06 -0400

    bin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .name("fresher")
   .description("A CLI utility to launch a fresh browser instance.")
-  .version("0.1.3")
+  .version("0.1.4")
   .requiredOption(
     '-b, --browser <"chromium" | "firefox" | "webkit">',
     "browser to launch"
